972381a929123b67335e3d066f51181721ade7fa
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2018-07-13 15:04:42 -0300

    arquivo conecta removido pois a conexão pdo é feita através classe Core\Db

--- conecta.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$conecta = mysql_connect("mysql.eduardo.machado.kinghost.net", "eduardo", "mudar5555") or print (mysql_error()); 
-mysql_select_db("eduardo", $conecta) or print(mysql_error()); 
-?>
