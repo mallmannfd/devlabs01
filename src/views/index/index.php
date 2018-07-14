e82f030cbd46fdd90f2183c58ac86be4ec836c12
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2018-07-13 21:49:37 -0300

    terminando o layout da página principal

--- src/views/index/index.php
+++ src/views/index/index.php
@@ -1,19 +1,26 @@
-<table class="table table-bordered table-striped">
-    <thead>
+<div class="jumbotron king-jumbotron text-center">
+    <h2 id="main-text" class="display-4">Devlabs</h2>
+</div>
+
+<a href="/add"><button class="btn btn-primary">Adicionar</button></a>
+
+<div class="main-table-div">
+    <table class="table table-bordered table-striped">
+        <thead>
         <tr>
             <th scope="col">
                 Nome
             </th>
         </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($funcionarios as $funcionario):  ?>
-        <tr>
-            <td>
-                <?="{$funcionario['nome_func']} {$funcionario['sobrenome_func']}";?>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-
+        </thead>
+        <tbody>
+        <?php foreach ($funcionarios as $funcionario):  ?>
+            <tr>
+                <td>
+                    <?="{$funcionario['nome_func']} {$funcionario['sobrenome_func']}";?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
