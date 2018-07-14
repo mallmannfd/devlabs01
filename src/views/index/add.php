03214d055bf85330d5a7ebf9de4167d1bd927d78
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2018-07-13 21:52:54 -0300

    Ajustando o link para o formulário

---
+++ src/views/index/add.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mallmann
+ * Date: 13/07/18
+ * Time: 21:50
+ */
--- src/views/index/index.php
+++ src/views/index/index.php
@@ -2,7 +2,7 @@
     <h2 id="main-text" class="display-4">Devlabs</h2>
 </div>
 
-<a href="/add"><button class="btn btn-primary">Adicionar</button></a>
+<a href="/index/add"><button class="btn btn-primary">Adicionar</button></a>
 
 <div class="main-table-div">
     <table class="table table-bordered table-striped">
